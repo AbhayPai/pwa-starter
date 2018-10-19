70fbb43333b506e73d6bbc217a8f1ec88a0962c7
Abhay Pai <pai.abhay8@gmail.com>
2018-10-19 11:19:44 +0800

    Update serviceworker.js
    updated naming convention for service worker and added index.html in path

--- serviceworker.js
+++ serviceworker.js
@@ -1,31 +1,31 @@
 (function () {
   "use strict";
-  var version = 'v2::';
+  var version = 'service-worker::';
   var pathToCache = [
-    '',
+    'index.html',
     'assets/css/index.css',
   ];
 
   self.addEventListener("install", function(event) {
-    console.log('WORKER: install event in progress.');
+    console.log('SERVICE WORKER: install event in progress.');
 
     event.waitUntil(
       caches
-        .open(version + 'fundamentals')
+        .open(version + 'demonstration')
         .then(function(cache) {
           return cache.addAll(pathToCache);
         })
         .then(function() {
-          console.log('WORKER: install completed');
+          console.log('SERVICE WORKER: install completed');
         })
     );
   });
 
   self.addEventListener("fetch", function(event) {
-    console.log('WORKER: fetch event in progress.');
+    console.log('SERVICE WORKER: fetch event in progress.');
 
     if (event.request.method !== 'GET') {
-      console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+      console.log('SERVICE WORKER: fetch event ignored.', event.request.method, event.request.url);
       return;
     }
     event.respondWith(
@@ -35,26 +35,26 @@
           var networked = fetch(event.request)
             .then(fetchedFromNetwork, unableToResolve)
             .catch(unableToResolve);
-            console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+            console.log('SERVICE WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
           return cached || networked;
 
           function fetchedFromNetwork(response) {
             var cacheCopy = response.clone();
-            console.log('WORKER: fetch response from network.', event.request.url);
+            console.log('SERVICE WORKER: fetch response from network.', event.request.url);
             caches
               .open(version + 'pages')
               .then(function add(cache) {
                 cache.put(event.request, cacheCopy);
               })
               .then(function() {
-                console.log('WORKER: fetch response stored in cache.', event.request.url);
+                console.log('SERVICE WORKER: fetch response stored in cache.', event.request.url);
               });
 
             return response;
           }
 
           function unableToResolve () {
-            console.log('WORKER: fetch request failed in both cache and network.');
+            console.log('SERVICE WORKER: fetch request failed in both cache and network.');
             return new Response('<h1>Service Unavailable</h1>', {
               status: 503,
               statusText: 'Service Unavailable',
@@ -68,7 +68,7 @@
   });
 
   self.addEventListener("activate", function(event) {
-    console.log('WORKER: activate event in progress.');
+    console.log('SERVICE WORKER: activate event in progress.');
 
     event.waitUntil(
       caches
@@ -85,7 +85,7 @@
           );
         })
         .then(function() {
-          console.log('WORKER: activate completed.');
+          console.log('SERVICE WORKER: activate completed.');
         })
     );
   });
